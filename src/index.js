1aacf3bf44fc46bc0a723017cc1d7636f27fb628
Michael Granados <michaelgranados@gmail.com>
2018-04-15 09:14:33 -0300

    applies routes
    Signed-off-by: Michael Granados <michaelgranados@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const Koa = require('koa');
+const Router = require('koa-router');
 
 const app = new Koa();
+const router = new Router();
 
-app.use(ctx => {
-  ctx.body = 'Hello World!';
+router.get('/', (ctx, next) => {
+  ctx.body = 'Hello world!!!';
 });
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
 
 app.listen(3000);
