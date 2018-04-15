73a7a94f14d4c33ad287b1b495ba1cd7f13a64a7
Michael Granados <michaelgranados@gmail.com>
2018-04-15 11:14:47 -0300

    creating form and serving static files
    Signed-off-by: Michael Granados <michaelgranados@gmail.com>

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const Koa = require('koa');
 const Router = require('koa-router');
 const views = require('koa-views');
+const static = require('koa-static');
 
 const app = new Koa();
 const router = new Router();
 
 app.use(views(`${__dirname}/views`, { extension: 'pug' }));
+app.use(static(`${__dirname}/static`));
 
 router.get('/', async (ctx) => {
   await ctx.render('index');
+});
+
+router.post('/login', async (ctx) => {
+  ctx.redirect('/');
 });
 
 app
