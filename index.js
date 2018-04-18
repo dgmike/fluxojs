5f1ac47f1ffa5048d7cc734d7f15024ce66a1c60
Michael Granados <michaelgranados@gmail.com>
2018-04-18 02:10:28 -0300

    remove console.log
    Signed-off-by: Michael Granados <michaelgranados@gmail.com>

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ if (!module.parent) {
   const port = process.env.PORT || '3000';
 
   app.listen(port, () => {
-    console.log(`Running on http://localhost:${port}`);
+    process.stdout.write(`Running on http://localhost:${port}\n`);
   });
 }
 
