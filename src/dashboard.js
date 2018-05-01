473382b306c7092b7f7022c189fa2778d70560da
Michael Granados <michaelgranados@gmail.com>
2018-05-01 19:30:43 -0300

    Fetching data from serverside

--- src/dashboard.js
+++ src/dashboard.js
@@ -1,50 +1,33 @@
 /* eslint-env node, browser */
 import Vue from 'vue';
+import moment from 'moment';
+import jquery from 'jquery';
 
 import AccountTable from './account-table.vue';
 
-new Vue({ // eslint-disable-line no-new
+const mainarea = new Vue({ // eslint-disable-line no-new
   el: '#mainarea',
   components: {
     AccountTable,
   },
   data: {
-    entrances: [
-      {
-        id: 1,
-        day: 1,
-        description: 'salário',
-        estimate: 1500,
-        real: 1402.23,
-        status: true,
-      },
-      {
-        id: 2,
-        day: 15,
-        description: 'salário (adiantamento)',
-        estimate: 1500,
-        real: 1402.23,
-        status: false,
-      },
-    ],
-    outputs: [
-      {
-        id: 3,
-        day: 21,
-        description: 'compras',
-        estimate: -1500,
-        real: -1402.23,
-        status: false,
-      },
-      {
-        id: 4,
-        day: 21,
-        description: 'compras',
-        estimate: -2320,
-        real: 0,
-        status: false,
-      },
-    ],
+    entrances: [],
+    outputs: [],
   },
   template: '<account-table :entrances="entrances" :outputs="outputs"></account-table>',
+  methods: {
+    fetch(date) {
+      const self = this;
+      const url = `/api/entrances?year=${date.year()}&month=${date.month()}`;
+
+      jquery
+        .get(url)
+        .then((data) => {
+          self.entrances = data.entrances.filter(e => e.estimate >= 0);
+          self.outputs = data.entrances.filter(e => e.estimate < 0);
+        });
+    }
+  }
 });
+
+mainarea.fetch(moment());
